1453b9d1f6cb673241961bfb61d143856161ce48
Riaz Uddin <mdriazuddin417@gmail.com>
2023-06-22 11:53:33 +0600

    update scan page complete

--- src/components/ScanBottomSheet.jsx
+++ src/components/ScanBottomSheet.jsx
@@ -1,4 +1,8 @@
 import React, { useEffect, useRef } from "react";
+import { code, profile } from "../assets/image";
+import { FaCopy } from "react-icons/fa";
+import { BsShare } from "react-icons/bs";
+import { IoMdRefresh } from "react-icons/io";
 
 const ScanBottomSheet = () => {
   const refBox = useRef(null);
@@ -46,10 +50,38 @@ const ScanBottomSheet = () => {
     <div>
       <div
         ref={refBox}
-        className="resizeable-box rounded-tl-[35px] rounded-tr-[35px] border border-gray-400 lg:w-[600px] md:w-[400px] w-[300px] shadow-inner bg-white"
+        className="resizeable-box rounded-tl-[35px] rounded-tr-[35px] border border-b-0 border-gray-400 lg:w-[600px] md:w-[400px] w-[300px] shadow-inner bg-white"
       >
-        <div className="sign "></div>
-        Resize me from the top
+        <div className="w-full bg-green-100 rounded-2xl mx-[10%] my-[10%] shadow-lg p-5 overflow-y-auto">
+          <div className="flex justify-center items-center gap-5 flex-col">
+            <div className="bg-primary p-1 rounded-full w-[90px] h-[90px]">
+              <img src={profile} alt="" className="w-full h-full" />
+            </div>
+            <h1 className="text-smallBlack text-[18px] font-bold">@Vedant</h1>
+            <div className="flex justify-center gap-5 items-center">
+              <p className="text-smallBlack text-[16px] font-bold">Knoct ID</p>
+              <p className="text-gray-500 text-[14px] font-medium">
+                did135416135166241641
+              </p>
+              <FaCopy
+                size={20}
+                className="text-gray-700 hover:text-black cursor-pointer"
+              />
+            </div>
+            <div className="w-[150px] aspect-[1] bg-white rounded p-2">
+              <img src={code} alt="" className="w-full " />
+            </div>
+            <div className="flex justify-center gap-7 items-center flex-col ">
+              <button className="btn btn-md btn-secondary text-white px-10 flex items-center gap-3">
+                <IoMdRefresh size={20} /> Refresh
+              </button>
+
+              <button className="btn btn-md btn-outline btn-primary px-10 flex items-center gap-3">
+                <BsShare size={20} /> Share
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
